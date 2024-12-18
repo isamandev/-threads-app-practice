c56f1d417fd98631fc06fd7995ee562cc3984325
Saman Abbasi <samanabbasi.ir@gmail.com>
2024-12-19 02:49:01 +0330

    HomePage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,7 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
-import Link from 'next/link';
-
 export default function Home() {
 	return (
-		<main>
-			<div className='relative flex gap-3'>
-				<SignedIn>
-					<UserButton />
-				</SignedIn>
-				<SignedOut>
-					<SignInButton>
-						<button className='px-4 py-2 rounded-full bg-[#131316] text-white text-sm font-semibold'>
-							Sign in
-						</button>
-					</SignInButton>
-				</SignedOut>
-			</div>
-		</main>
+		<>
+			<h1 className='head-text text-left'>Home</h1>
+		</>
 	);
 }
